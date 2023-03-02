5f72de5aedf3c44aa40eb0f059902534838d92ed
FcoJavierGlez <FJGS_86@hotmail.com>
2023-03-02 08:22:32 +0100

    Añadida ruta data

--- src/enigma/Main.java
+++ src/enigma/Main.java
@@ -14,21 +14,21 @@ public class Main {
             // GENERACIÓN LLAVE \\
 //            c.generaLlave();
 //            c.seleccionaLlave(1);
-//            c.exportaLlave(KEY_NAME);
+//            c.exportaLlave("data\\keys\\" + KEY_NAME);
 
             // IMPORTAR LLAVE \\
-            c.importaLlave("keys\\" + KEY_NAME + ".kyph");
+            c.importaLlave("data\\keys\\" + KEY_NAME + ".kyph");
             c.seleccionaLlave(1);
 
             // CIFRAR FICHERO \\
-            Texto.importaFichero("messages\\mensaje.txt");
+            Texto.importaFichero("data\\messages\\mensaje.txt");
             c.encriptaTexto();
-            Texto.exportaFichero("messages\\mensaje_cifrado.txt");
+            Texto.exportaFichero("data\\messages\\mensaje_cifrado.txt");
 
             // DESCIFRAR FICHERO \\
-            Texto.importaFichero("messages\\mensaje_cifrado.txt");
+            Texto.importaFichero("data\\messages\\mensaje_cifrado.txt");
             c.desencriptaTexto();
-            Texto.exportaFichero("messages\\mensaje_descifrado.txt");
+            Texto.exportaFichero("data\\messages\\mensaje_descifrado.txt");
         }
         catch (limiteLlavesException e) {} catch (IOException e) {}
         catch (ErrorSeleccionLlave e) {}
